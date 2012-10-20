30e42a56ee3ab3379f67ff0412eb09889277ebf4
Sachin <sachin.arox@gmail.com>
2012-10-20 17:30:36 +0530

    Hello.java added and used in MainActivity.java

---
+++ HelloWorld/src/com/demo/helloworld/Hello.java
@@ -0,0 +1,9 @@
+package com.demo.helloworld;
+
+public class Hello {
+
+	String classname = "Hello";
+	String getClassName(){
+		return classname;
+	}
+}
--- HelloWorld/src/com/demo/helloworld/MainActivity.java
+++ HelloWorld/src/com/demo/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.demo.helloworld;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,new Hello().getClassName(),Toast.LENGTH_SHORT).show();
     }
 
     @Override
